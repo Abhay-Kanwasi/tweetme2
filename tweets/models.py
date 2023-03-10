98859855aa5c7c3c365d4a167139007d68838045
Abhay Kanwasi <78997764+Abhay-Coder001@users.noreply.github.com>
2023-03-10 22:51:05 +0530

    tweet app config.

---
+++ tweets/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Tweet(models.Model):
+    # We can have three types of data as tweets : id text image
+    #It also created a column name id by default in our database
+    #id = models.AutoField(primary_key=True) 
+
+    #It will create a column name content in our database
+    content = models.TextField(blank=True, null=True)
+
+    # It will create a image column in our database
+    image = models.FileField(upload_to="images/", blank=True, null=True) #blank = not required in django | null = not required in database
