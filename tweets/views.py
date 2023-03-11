9099f36fdf77f9d2ad96e81a0967dc18eb730992
Abhay Kanwasi <78997764+Abhay-Coder001@users.noreply.github.com>
2023-03-11 21:55:43 +0530

    handle dynamic url

--- tweets/views.py
+++ tweets/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse,Http404
+from .models import Tweet
 # Create your views here.
 def home_view(requests,*args,**kwargs):
     return HttpResponse("<h1>Hello World</h1>") # But how django will know so we add it to urls.py
 
 def tweet_detail_view(requests, tweet_id , *args,**kwargs):
-    return HttpResponse(f"<h1>Hello {tweet_id}</h1>") # But how django will know so we add it to urls.py
+    try:
+        obj = Tweet.objects.get(id = tweet_id)
+    except:
+        raise Http404
+        return HttpResponse(f"<h1>Konichiwa {tweet_id} - {obj.content}</h1>") # But how django will know so we add it to urls.py
 
