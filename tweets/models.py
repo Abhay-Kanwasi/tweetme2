6ec38df68a56495ae9651a58c2a0064f4007f7c1
Abhay Kanwasi <78997764+Abhay-Coder001@users.noreply.github.com>
2023-03-18 21:53:09 +0530

    user creation and tweet handling

--- tweets/models.py
+++ tweets/models.py
@@ -1,12 +1,21 @@
 import random
 from django.db import models
 
+#user creation 
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
+
 # Create your models here.
 
 class Tweet(models.Model):
     # We can have three types of data as tweets : id text image
     #It also created a column name id by default in our database
     #id = models.AutoField(primary_key=True) 
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+     #ForeignKey : user can do many tweets but one tweet belong to it's own user.
+     #CASCADE :If user is deleted then all the tweets by user will also be deleted.
 
     #It will create a column name content in our database
     content = models.TextField(blank=True, null=True)
