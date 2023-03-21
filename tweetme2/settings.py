016829c1ad33329ca6a7278c5bc72caa56dc3f71
Abhay Kanwasi <78997764+Abhay-Coder001@users.noreply.github.com>
2023-03-22 00:53:27 +0530

    Rest_Framework added to settings.py

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -126,6 +126,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES' : [
+        'rest_framework.authentication.SessionAuthentication'
+    ],
+    'DEFAULT_RENDERER_CLASSES' : [
+        'rest_framework.renderers.JSONRenderer',
+        # 'rest_framework.renderers.BrowsableAPIRenderer'
+    ]
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
