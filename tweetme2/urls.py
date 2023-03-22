0dbccdf9230b8de9fc351ecd5abf8fa256a15cf1
Abhay Kanwasi <78997764+Abhay-Coder001@users.noreply.github.com>
2023-03-22 22:13:30 +0530

    tweet_create_view contected to django through url

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -18,8 +18,11 @@ from django.urls import path, re_path
 from tweets.views import home_view, tweet_detail_view, tweet_create_view
 
 from tweets.views import (
-    home_view, tweet_detail_view, tweet_list_view,
-     tweet_create_view,
+    home_view, 
+    tweet_detail_view,
+    tweet_list_view,
+    tweet_delete_view,
+    tweet_create_view,
 )
 
 urlpatterns = [
@@ -27,5 +30,6 @@ urlpatterns = [
     path('', home_view), #That's how django know that we created a home_view
     re_path('create-tweet',tweet_create_view),
     path('tweets',tweet_list_view),
-    path('tweets/<int:tweet_id>',tweet_detail_view)
+    path('tweets/<int:tweet_id>',tweet_detail_view),
+    path('api/tweets/<int:tweet_id>/delete',tweet_delete_view),
 ]
