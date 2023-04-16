caa4a5da6e2e963c89643b204d0ae4eca0bba7bb
Abhay Kanwasi <78997764+Abhay-Coder001@users.noreply.github.com>
2023-04-16 11:35:22 +0530

    Ajax lookup via XHR in Reactjs

--- tweetme2-web/src/App.js
+++ tweetme2-web/src/App.js
@@ -3,14 +3,45 @@ import React, { useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// function loadTweets is imported(copied) form home.html
+function loadTweets(callback) {
+  // initial setup
+  const xhr = new XMLHttpRequest()
+  const method = 'GET' //"POST"
+  const url = " http://127.0.0.1:8000/api/tweets/"
+  const responseType = "json"
 
+  // perform the request
+  xhr.responseType = responseType  //set it's own response type to initial response type
+  xhr.open(method, url) //Open up the request with this method and this url
+  //now handle this whenever it's loded
+  xhr.onload = function(){
+    callback(xhr.response, xhr.status)
+  } // return xhr response and it's status
+  xhr.onerror = function (e) {
+    console.log(e)
+    callback({"message" : "Request was the error"}, 400)
+  }
+  xhr.send() // triger this request
+}
+  
 function App() {
   const [tweets, setTweets] = useState([{ content: 123 }]) //with this we can use these inside of my component using curly brackets({})
   
+
   useEffect(() => {
     // do my lookup
-    const tweetsItems = [{"content": 123}, {"content" : "Hello World"}]
-    setTweets(tweetsItems)
+    const myCallback = (response, status) => {
+      console.log(response, status)
+      if (status === 200) {
+        setTweets(response) //tweetItems is nothing but response
+      }
+      else {
+        alert("There was an error") // This will get us a pop-up message (localhost: 3000 says (whaterver inside of alert))
+      }
+    }
+    loadTweets(myCallback)
+    
   }, [])
   return (
     <div className="App">
