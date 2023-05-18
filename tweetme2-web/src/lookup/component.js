bfd7af1fa56e411529c7fd7579ecfa869cb7a195
Abhay Kanwasi <78997764+Abhay-Coder001@users.noreply.github.com>
2023-05-18 11:38:40 +0530

    committed

--- tweetme2-web/src/lookup/component.js
+++ tweetme2-web/src/lookup/component.js
@@ -1,4 +1,18 @@
-function lookup(method, endpoint, callback, data) {
+function getCookie(name){
+    var cookieValue = null;
+    if(document.cookie && document.cookie !== ''){
+        var cookies = document.cookie.split(';');
+        for (var i=0; i<cookies.length; i++){
+            var cookie = cookies[i].trim();
+            if(cookie.substring(0,name.length+1)===(name + '=')){
+                cookieValue = decodeURIComponent(cookie.substring(name.length+1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+  function lookup(method, endpoint, callback, data) {
   let jsonData;
   if (data) {
     jsonData = JSON.stringify(data)
@@ -6,11 +20,16 @@ function lookup(method, endpoint, callback, data) {
   const xhr = new XMLHttpRequest();
     const url = `http://localhost:8000/api${endpoint}/` // Changing this url so our react app can have some sort of connection with django server
     const responseType = "json";
-  
-    // perform the request
-    xhr.responseType = responseType; //set it's own response type to initial response type
-    xhr.open(method, url); //Open up the request with this method and this url
-    //now handle this whenever it's loded
+    const csrftoken = getCookie('csrftoken');
+    xhr.open(method,url)
+    xhr.setRequestHeader("Content-Type","application/json")
+
+    if (csrftoken) {
+    xhr.setRequestHeader("HTTP_X_REQUESTED_WITH","XMLHttpRequest")
+    xhr.setRequestHeader("X-Requested-With","XMLHttpRequest")
+    xhr.setRequestHeader("X-CSRFToken", csrftoken)
+    }
+
     xhr.onload = function () {
       callback(xhr.response, xhr.status);
     }; // return xhr response and it's status
